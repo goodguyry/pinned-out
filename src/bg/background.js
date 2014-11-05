a4eff6e145477e5c22d6b44dceda349660219dca
Ryan Domingue <ryan.domingue@storyworldwide.com>
2014-11-05 11:18:04 -0800

    Added message sending from inject to background to receive tab info

--- src/bg/background.js
+++ src/bg/background.js
@@ -1,95 +1,18 @@
-// if you checked "fancy-settings" in extensionizr.com, uncomment this lines
-
-// var settings = new Store("settings", {
-//     "sample_setting": "This is how you use Store.js to remember values"
-// });
-
-
-
-
-
-
-/*
- * Receive the search text from popup.js
- * See http://developer.chrome.com/extensions/messaging.html
+/**
+ * Send info about the tab to the content script
+ *
+ * pinnedOut.scriptResponse: Dummy text to print to the console
+ * pinnedOut.tabIndex: The tab's index, used to create a new tab
+ * pinnedOut.pinnedState: Whether or not the tab is pinned
  */
 chrome.extension.onMessage.addListener(
   function(request, sender, sendResponse) {
-
-    var pinnedState = null;
-
-    chrome.tabs.query({currentWindow: true, active: true}, function(tabs) {
-
-      pinnedState = tabs[0].pinned;
-
-      // Sent from popup.js
-      if (request.scriptMessage === 'ready') {
-        console.log('inject.js', request.scriptMessage);
-        // Get the current tab's index and send the search text along to the search handler
-      }
-
-    });
-
-    window.setTimeout(function() {
-      console.log('pinnedState', pinnedState);
-
-      if (request.scriptMessage !== '') {
-        sendResponse({scriptResponse: pinnedState});
-      }
-
-    }, 100);
-
+    var pinnedOut = {};
+    pinnedOut.scriptResponse = "response received";
+    pinnedOut.tabIndex = sender.tab.index;
+    pinnedOut.pinnedState = sender.tab.pinned;
+    console.log('inject.js:', request.message);
+    if (request.message === 'ready') {
+      sendResponse(pinnedOut);
+    }
   });
-
-
-
-/*
- * Receive the search text from popup.js
- * See http://developer.chrome.com/extensions/messaging.html
- */
-// chrome.extension.onMessage.addListener(
-//   function(request, sender, sendResponse) {
-
-//     var respondToInject = {
-//       scriptResponse: 'Do work...'
-//     };
-
-//     // Sent from popup.js
-//     if (request.scriptMessage === 'ready') {
-//       console.log('inject.js', request.scriptMessage);
-//       // Get the current tab's index and send the search text along to the search handler
-//       chrome.tabs.query({currentWindow: true, active: true}, function(tabs) {
-//         // for (var i = 0; i < tabs.length; i++) {
-//         // respondToInject.scriptResponse = tabs[0].pinned;
-//         console.log('pinnedState', tabs[0].pinned);
-//         sendResponse({scriptResponse: tabs[0].pinned});
-//         // }
-//       });
-//       // chrome.tabs.query({currentWindow: true, active: true}, function (tabs) {
-//       //   // Do something here
-//       //   // this tab is tabs[0].index
-//       // });
-//       // if (request.scriptMessage !== '') {
-//       // }
-//     }
-
-//   });
-
-
-
-
-
-
-
-
-
-
-//example of using a message handler from the inject scripts
-// chrome.extension.onMessage.addListener(
-//   function(request, sender, sendResponse) {
-//   	chrome.pageAction.show(sender.tab.id);
-//     sendResponse();
-//   });
-
-
-
--- src/inject/inject.js
+++ src/inject/inject.js
@@ -1,25 +1,21 @@
-var manipulateAnchors = function() {
-  var anchors = document.getElementsByTagName('a');
-  for (var a = 0; a < anchors.length; a++) {
-    anchors[a].style.backgroundColor = 'green';
-  }
-};
+/**
+ * Let background.js know we're ready for tab info
+ * Receive info about the tab from background.js
+ *
+ * response.scriptResponse: Dummy text to print to the console
+ * response.tabIndex: The tab's index, used to create a new tab
+ * response.pinnedState: Whether or not the tab is pinned
+ */
+chrome.extension.sendMessage({message: 'ready'}, function(response) {
+  var readyStateCheckInterval = setInterval(function() {
+    if (document.readyState === "complete") {
+      clearInterval(readyStateCheckInterval);
 
-chrome.extension.sendMessage({scriptMessage: 'ready'},
-  function(response) {
-    var readyStateCheckInterval = setInterval(function() {
-      if (document.readyState === "complete") {
-        clearInterval(readyStateCheckInterval);
+      // This part of the script triggers when page is done loading
+      console.log('background.js:', response.scriptResponse);
+      console.log('pinned:', response.pinnedState);
+      console.log('index:', response.tabIndex);
 
-        // ----------------------------------------------------------
-        // This part of the script triggers when page is done loading
-        if (response.scriptResponse !=='') {
-          console.log('response', response.scriptResponse);
-          // console.log('pinnedState', response.pinnedState);
-          manipulateAnchors();
-        }
-        // ----------------------------------------------------------
-
-      }
-    }, 10);
+    }
+  }, 10);
   });
