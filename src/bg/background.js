98fc3ff355f9305ac03488959d7f2afedfa3edf5
Ryan Domingue <ryan@ryandomingue.com>
2014-11-07 21:55:25 -0800

    Removed old message passing code
    For now, tabs.onUpdate is the way to go

--- src/bg/background.js
+++ src/bg/background.js
@@ -1,24 +1,4 @@
 /**
- * Listen for a signal from the content script
- * Send info about the tab to the content script
- *
- * pinnedOut.scriptResponse: Dummy text to print to the console
- * pinnedOut.tabIndex: The tab's index, used to create a new tab
- * pinnedOut.pinnedState: Whether or not the tab is pinned
- */
-chrome.extension.onMessage.addListener(
-  function(request, sender, sendResponse) {
-    var pinnedOut = {};
-    pinnedOut.scriptResponse = 'response received';
-    pinnedOut.tabIndex = sender.tab.index;
-    pinnedOut.pinnedState = sender.tab.pinned;
-    console.log('inject.js:', request.message);
-    if (request.message === 'ready') {
-      // sendResponse(pinnedOut);
-    }
-  });
-
-/**
  * Listen for tab updates
  * Send the information to the content script
  *
--- src/inject/inject.js
+++ src/inject/inject.js
@@ -66,36 +66,6 @@ pinnedLinkOut.manipulateAnchors = function(pinned) {
 };
 
 /**
- * Let the background script know we're ready for tab info
- * Receive info about the tab from the background script
- *
- * response.scriptResponse: Dummy text to print to the console
- * response.tabIndex: The tab's index, used to create a new tab
- * response.pinnedState: Whether or not the tab is pinned
- */
-chrome.extension.sendMessage({message: 'ready'}, function(response) {
-  var readyStateCheckInterval = setInterval(function() {
-    if (document.readyState === "complete") {
-      clearInterval(readyStateCheckInterval);
-
-      // This part of the script triggers when page is done loading
-      console.log('background.js:', response.scriptResponse);
-      console.log('pinned:', response.pinnedState);
-      console.log('index:', response.tabIndex);
-
-      if (response.pinnedState) {
-        // The tab is pinned
-        pinnedLinkOut.manipulateAnchors(true);
-      } else {
-        // The tab is not pinned
-        // Do nothing
-      }
-
-    }
-  }, 10);
-  });
-
-/**
  * Receive message from the background script regarding tab updates
  *
  * @param {Object}  request.changed The tab update event
