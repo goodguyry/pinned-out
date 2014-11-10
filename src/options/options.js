cced27ef237136e685ff4485bd66989211682799
Ryan Domingue <ryan@ryandomingue.com>
2014-11-09 19:18:57 -0800

    Removed third (internal-only) option for link behavior

--- src/options/options.js
+++ src/options/options.js
@@ -1,22 +1,18 @@
 /**
  * Retrieve the stored value
- * Restore the saved state by matching the radio button's value to the stored value
+ * Restore the saved state by matching the stored value to the radio button's value
  * If nothing is saved, or the saved state is somehow incompatible, 'default' is stored
  */
 var storedBehavior = localStorage.getItem('behavior');
 var radios = document.getElementsByName('behavior');
-switch (storedBehavior) {
-  case radios[1].value:
-    radios[1].checked = true;
-    break;
-  case radios[2].value:
-    radios[2].checked = true;
-    break;
-  default:
-    radios[0].checked = true;
-    localStorage.setItem('behavior', 'default');
-    break;
+if (storedBehavior === radios[1].value) {
+  radios[1].checked = true;
+} else {
+  radios[0].checked = true;
+  // In case nothing is in localStorage
+  localStorage.setItem('behavior', 'default');
 }
+
 
 /**
  * Save the clicked radio button's value to localStorage
